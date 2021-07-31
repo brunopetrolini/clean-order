a01f0b75ca1eb0d0ed7583544c70cecabe505956
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-07-30 21:10:16 -0300

    test: ensure create an order if 3 items is provided

--- src/Order.test.ts
+++ src/Order.test.ts
@@ -5,4 +5,14 @@ describe("Order", () => {
     const cpf = "111.111.111-11";
     expect(() => new Order(cpf)).toThrow(new Error("Invalid CPF"));
   });
+
+  test("Should create a order with 3 items", () => {
+    const cpf = "778.278.412-36";
+    const order = new Order(cpf);
+    order.addItem("Guitarra", 1000, 2);
+    order.addItem("Amplificador", 5000, 1);
+    order.addItem("Cabo", 30, 3);
+    const total = order.getTotal();
+    expect(total).toBe(7090);
+  });
 });
