503f449846ec29dea4b0f3c3f1f64e179c3a2af3
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-07-30 21:46:03 -0300

    feat: ensure create a order with an discount coupon

--- src/Order.ts
+++ src/Order.ts
@@ -1,9 +1,11 @@
+import Coupon from "./Coupon";
 import Cpf from "./Cpf";
 import OrderItem from "./OrderItem";
 
 export default class Order {
   cpf: Cpf;
   items: OrderItem[];
+  coupon?: Coupon;
 
   constructor(cpf: string) {
     this.cpf = new Cpf(cpf);
@@ -14,10 +16,17 @@ export default class Order {
     this.items.push(new OrderItem(description, price, quantity));
   }
 
+  addCoupon(coupon: Coupon): void {
+    this.coupon = coupon;
+  }
+
   getTotal(): number {
     let total = 0;
     for (const orderItem of this.items) {
       total += orderItem.getItemTotal();
+    }
+    if (this.coupon) {
+      total -= (total * this.coupon.percentage) / 100;
     }
     return total;
   }
