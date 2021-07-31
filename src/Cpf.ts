21f10565a4adb8c69bac5f0c192c96009c1f83e6
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-07-30 21:29:13 -0300

    refactor: move cpf validation to its own file

---
+++ src/Cpf.ts
@@ -0,0 +1,60 @@
+export default class Cpf {
+  value: string;
+
+  constructor(value: string) {
+    if (!this.validate(value)) throw new Error("Invalid CPF");
+    this.value = value;
+  }
+
+  FACTOR_DIGIT_1 = 10;
+  FACTOR_DIGIT_2 = 11;
+  MAX_DIGITS_1 = 9;
+  MAX_DIGITS_2 = 10;
+
+  validate(cpf = ""): boolean {
+    cpf = this.extractDigits(cpf);
+    if (this.isInvalidLength(cpf)) return false;
+    if (this.isBlocked(cpf)) return false;
+    const digit1 = this.calculateDigit(
+      cpf,
+      this.FACTOR_DIGIT_1,
+      this.MAX_DIGITS_1
+    );
+    const digit2 = this.calculateDigit(
+      cpf,
+      this.FACTOR_DIGIT_2,
+      this.MAX_DIGITS_2
+    );
+    let calculatedCheckDigit = `${digit1}${digit2}`;
+    return this.getCheckDigit(cpf) == calculatedCheckDigit;
+  }
+
+  extractDigits(cpf: string): string {
+    return cpf.replace(/\D/g, "");
+  }
+
+  isInvalidLength(cpf: string): boolean {
+    return cpf.length !== 11;
+  }
+
+  isBlocked(cpf: string): boolean {
+    const [digit1] = cpf;
+    return cpf.split("").every((digit) => digit === digit1);
+  }
+
+  calculateDigit(cpf: string, factor: number, max: number): number {
+    let total = 0;
+    for (const digit of this.toDigitArray(cpf).slice(0, max)) {
+      total += digit * factor--;
+    }
+    return total % 11 < 2 ? 0 : 11 - (total % 11);
+  }
+
+  toDigitArray(cpf: string): number[] {
+    return [...cpf].map((digit) => parseInt(digit));
+  }
+
+  getCheckDigit(cpf: string): string {
+    return cpf.slice(9);
+  }
+}
--- src/Order.ts
+++ src/Order.ts
@@ -1,12 +1,12 @@
+import Cpf from "./Cpf";
 import OrderItem from "./OrderItem";
 
 export default class Order {
-  cpf: string;
+  cpf: Cpf;
   items: OrderItem[];
 
   constructor(cpf: string) {
-    if (!this.validate(cpf)) throw new Error("Invalid CPF");
-    this.cpf = cpf;
+    this.cpf = new Cpf(cpf);
     this.items = [];
   }
 
@@ -20,57 +20,5 @@ export default class Order {
       total += orderItem.getItemTotal();
     }
     return total;
-  }
-
-  FACTOR_DIGIT_1 = 10;
-  FACTOR_DIGIT_2 = 11;
-  MAX_DIGITS_1 = 9;
-  MAX_DIGITS_2 = 10;
-
-  validate(cpf = "") {
-    cpf = this.extractDigits(cpf);
-    if (this.isInvalidLength(cpf)) return false;
-    if (this.isBlocked(cpf)) return false;
-    const digit1 = this.calculateDigit(
-      cpf,
-      this.FACTOR_DIGIT_1,
-      this.MAX_DIGITS_1
-    );
-    const digit2 = this.calculateDigit(
-      cpf,
-      this.FACTOR_DIGIT_2,
-      this.MAX_DIGITS_2
-    );
-    let calculatedCheckDigit = `${digit1}${digit2}`;
-    return this.getCheckDigit(cpf) == calculatedCheckDigit;
-  }
-
-  extractDigits(cpf: string) {
-    return cpf.replace(/\D/g, "");
-  }
-
-  isInvalidLength(cpf: string) {
-    return cpf.length !== 11;
-  }
-
-  isBlocked(cpf: string) {
-    const [digit1] = cpf;
-    return cpf.split("").every((digit) => digit === digit1);
-  }
-
-  calculateDigit(cpf: string, factor: number, max: number) {
-    let total = 0;
-    for (const digit of this.toDigitArray(cpf).slice(0, max)) {
-      total += digit * factor--;
-    }
-    return total % 11 < 2 ? 0 : 11 - (total % 11);
-  }
-
-  toDigitArray(cpf: string) {
-    return [...cpf].map((digit) => parseInt(digit));
-  }
-
-  getCheckDigit(cpf: string) {
-    return cpf.slice(9);
   }
 }
