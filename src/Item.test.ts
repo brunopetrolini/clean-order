2108fba480f3540bde4975868afbc63f2a660b89
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-08-05 22:21:03 -0300

    feat: ensure calculate the density of a item

--- src/Item.test.ts
+++ src/Item.test.ts
@@ -6,4 +6,10 @@ describe("Item", () => {
     const volume = item.getVolume();
     expect(volume).toBe(0.125);
   });
+
+  test("Should calculate the density of a item", () => {
+    const item = new Item("1", "Amplificador", 5000, 50, 50, 50, 22);
+    const density = item.getDensity();
+    expect(density).toBe(176);
+  });
 });
--- src/Item.ts
+++ src/Item.ts
@@ -28,4 +28,8 @@ export default class Item {
   getVolume() {
     return (this.width / 100) * (this.height / 100) * (this.length / 100);
   }
+
+  getDensity() {
+    return this.weight / this.getVolume();
+  }
 }
