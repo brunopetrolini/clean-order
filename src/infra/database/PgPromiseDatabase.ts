6c4976b5081aacfc70887f087b3f9da2066b3803
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-09-16 14:50:16 -0300

    fix: update postgres connection url

--- src/infra/database/PgPromiseDatabase.ts
+++ src/infra/database/PgPromiseDatabase.ts
@@ -2,29 +2,29 @@ import pgp from "pg-promise";
 import Database from "../database/Database";
 
 export default class PgPromiseDatabase implements Database {
-    private pgp: any;
-    static instance: PgPromiseDatabase;
+  private pgp: any;
+  static instance: PgPromiseDatabase;
 
-    private constructor () {
-        this.pgp = pgp()("postgres://postgres:123456@localhost:5432/app");
-    }
+  private constructor() {
+    this.pgp = pgp()("postgres://postgres:123456@172.17.0.2:5432/app");
+  }
 
-    static getInstance() {
-        if (!PgPromiseDatabase.instance) {
-            PgPromiseDatabase.instance = new PgPromiseDatabase();
-        }
-        return PgPromiseDatabase.instance;
+  static getInstance() {
+    if (!PgPromiseDatabase.instance) {
+      PgPromiseDatabase.instance = new PgPromiseDatabase();
     }
+    return PgPromiseDatabase.instance;
+  }
 
-    many(query: string, parameters: any) {
-        return this.pgp.query(query, parameters);
-    }
-    
-    one(query: string, parameters: any) {
-        return this.pgp.oneOrNone(query, parameters);
-    }
+  many(query: string, parameters: any) {
+    return this.pgp.query(query, parameters);
+  }
 
-    none(query: string, parameters: any): void {
-        return this.pgp.none(query, parameters);
-    }
+  one(query: string, parameters: any) {
+    return this.pgp.oneOrNone(query, parameters);
+  }
+
+  none(query: string, parameters: any): void {
+    return this.pgp.none(query, parameters);
+  }
 }
